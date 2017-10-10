6097ab3dabfdc3d4d24f0856179e4767822650ed
Peter Destasio <peterdestasio@ilxmac005.intelex.com>
2017-10-10 17:13:36 -0400

    First Commit

---
+++ src/app.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import Movies from './Movies';
+import {
+  Navigator,
+} from 'react-native-deprecated-custom-components';
+import Confirmation from './Confirmation';
+
+
+const RouteMapper = (route, navigator) => {
+  if (route.name === 'movies') {
+    return (
+      <Movies navigator={navigator} />
+    );
+  } else if (route.name === 'confirmation') {
+    return (
+      <Confirmation code={route.code} navigator={navigator} />
+    );
+  }
+};
+
+export default class App extends Component {
+  render() {
+    return (
+      <Navigator
+        // Default to movies route
+        initialRoute={{ name: 'movies' }}
+        // Use FloatFromBottom transition between screens
+        configureScene={(route, routeStack) => Navigator.SceneConfigs.FloatFromBottom}
+        // Pass a route mapper functions
+        renderScene={RouteMapper}
+      />
+    );
+  }
+  
+}
