47e71b167656edd3616f5277584b6b13a4b88c08
Pierluigi <pierluigi.destasio@sisal.it>
2021-10-15 17:04:13 +0200

    Update to latest react native

--- src/app.js
+++ src/app.js
@@ -1,35 +1,48 @@
-import React, { Component } from 'react';
+import * as React from 'react';
 import Movies from './Movies';
-import {
-  Navigator,
-} from 'react-native-deprecated-custom-components';
+//import Navigator from 'react-native-deprecated-custom-components';
 import Confirmation from './Confirmation';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 
-const RouteMapper = (route, navigator) => {
-  if (route.name === 'movies') {
-    return (
-      <Movies navigator={navigator} />
-    );
-  } else if (route.name === 'confirmation') {
-    return (
-      <Confirmation code={route.code} navigator={navigator} />
-    );
-  }
-};
+// const RouteMapper = (route, navigator) => {
+//   if (route.name === 'movies') {
+//     return (
+//       <Movies navigator={navigator} />
+//     );
+//   } else if (route.name === 'confirmation') {
+//     return (
+//       <Confirmation code={route.code} navigator={navigator} />
+//     );
+//   }
+// };
 
-export default class App extends Component {
-  render() {
+export default function App() {
     return (
-      <Navigator
-        // Default to movies route
-        initialRoute={{ name: 'movies' }}
-        // Use FloatFromBottom transition between screens
-        configureScene={(route, routeStack) => Navigator.SceneConfigs.FloatFromBottom}
-        // Pass a route mapper functions
-        renderScene={RouteMapper}
-      />
+      // <Navigator
+      //   // Default to movies route
+      //   initialRoute={{ name: 'movies' }}
+      //   // Use FloatFromBottom transition between screens
+      //   configureScene={(route, routeStack) => Navigator.SceneConfigs.FloatFromBottom}
+      //   // Pass a route mapper functions
+      //   renderScene={RouteMapper}
+      // />
+      <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
     );
-  }
   
 }
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+        <Stack.Screen name="movies" component={Movies} />
+        <Stack.Screen name="confirmation" component={Confirmation} />
+      </Stack.Navigator>
+  );
+}
+
